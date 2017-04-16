91ede40bd600aad5cffea5dee5602d4abf53ad0c
zookfish <861712654@qq.com>
2017-04-16 17:56:55 +0800

    Initial commit

---
+++ Doc.md
@@ -0,0 +1,5 @@
+###　快捷键
+* alt + insert  选中文件夹新建一个类
+* shift + F6 重命名文件
+* ctrl + alt + T 选中代码块添加try catch
+
---
+++ src/com/yunux/thread/Run.java
@@ -0,0 +1,22 @@
+package com.yunux.thread;
+
+/**
+ * Created by Administrator on 2017/4/16.
+ */
+public class Run {
+    public static void main(String[] args) {
+        try {
+            MyThread myThread= new MyThread();
+            myThread.setName("mythread");
+            myThread.start();   // 线程就绪  start的顺序不是代码执行的顺序
+            for (int i =0;i<10;i++) {
+                int time = (int)(Math.random()*1000);
+                Thread.sleep(time);
+                System.out.println("main=" + Thread.currentThread().getName());
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
